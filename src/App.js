aea7339b896580c1c636e7ebfb86fe4c4bbdf0d3
Viktoria Sykalova <vikasykalova@gmail.com>
2022-03-31 13:38:03 +0300

    добавили свой хук и сделали поле для ввода пунктов списка

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TodoList from './Todo/ToDoList'
 import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -24,11 +25,21 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+      title,
+      id: Date.now(),
+      completed: false
+      }
+    ]))
+  }
+
   return (
     <Context.Provider value={{removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-
+        <AddTodo onCreate={addTodo} />
         {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>
         ) : (
         <p>No todos!</p>)}
---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import PropTypes from 'prop-types';
+
+/* создаем новый хук */
+function useInputValue (defaultValue = '') {
+    const [value, setValue] = useState(defaultValue)
+
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
+function AddTodo ({onCreate}) {
+    const input = useInputValue('')
+
+    const [value, setValue] = useState('')
+
+    function submitHandler(event) {
+        event.preventDefault()
+
+        if (input.value().trim()) {
+            onCreate(input.value())
+            input.clear()
+        }
+    }
+
+    return (
+        <form style={{ marginBottom: '1rem' }} onSubmit={submitHandler}>
+            <input {...input.bind} />
+            <button type="submit">Add todo</button>
+        </form>
+    )
+}
+
+AddTodo.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo
